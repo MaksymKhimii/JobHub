a4c6f4eff03a14a4488ad2c1a15c397d5da3f3b6
Maksym Khimii <meizovicmaks@gmail.com>
2023-03-15 21:31:21 +0200

    filling the db and preparing test photos

---
+++ src/main/java/ua/khimii/jobhub/model/LanguageLevel.java
@@ -0,0 +1,21 @@
+package ua.khimii.jobhub.model;
+
+public enum LanguageLevel {
+    BEGINNER,
+
+    ELEMENTARY,
+
+    PRE_INTERMEDIATE,
+
+    INTERMEDIATE,
+
+    UPPER_INTERMEDIATE,
+
+    ADVANCED,
+
+    PROFICIENCY;
+
+    public String getDbValue() {
+        return name().toLowerCase();
+    }
+}
